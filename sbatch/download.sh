97e742d81349844684f44ecebc9a311fcc58ef34
sdmuhsin <sdmuhsin@gmail.com>
2025-02-09 17:34:42 -0600

    Full download script

---
+++ sbatch/download.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define arrays for models and datasets
+models=(
+    "facebook/opt-1.3B" "facebook/opt-2.7B" "facebook/opt-6.7b" 
+    "facebook/opt-13b" "facebook/opt-30b" "facebook/opt-66b"
+    "huggyllama/llama-7b" "huggyllama/llama-13b" "huggyllama/llama-30b" "huggyllama/llama-65b"
+)
+
+datasets=("ptb" "wikitext2" "c4")
+
+# Iterate over each model and dataset
+for model in "${models[@]}"; do
+    for dataset in "${datasets[@]}"; do
+        echo "Running experiment with model: $model and dataset: $dataset"
+        python3 run.py "$model" "$dataset" braq --blocksize 128 --salient_metric hessian --device="cpu" --just_download
+    done
+done
+
--- sbatch/trial.sh
+++
@@ -1,1 +0,0 @@
-python3 run.py facebook/opt-6.7b ptb braq --block_size 128 --salient_metric hessian --device="cpu" --just_download
